25bcd18eff98317776d7a0b877087ae912e4412e
vadim khakham <vadim.khakham@gmail.com>
2016-04-02 22:17:43 +0300

    delete files

--- hw1/grade.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-$(diff queries_out.txt src/main/resources/queries_out.txt) > /dev/null
-if [ $? -eq 0 ]; then echo "You output is correct. PERFECT MATCH!!!"; else echo "Incorrect output, see the differences: ";diff queries_out.txt src/main/resources/queries_out.txt; fi;
